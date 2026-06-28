80c2b45398ec1dd84943c25b05e36fcab2c36c12
Amaan Pasha <amaanpasha23@gmail.com>
2026-06-28 16:22:37 +0530

    now the whole backend is completed after this

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -4,9 +4,31 @@ const router = express.Router();
 
 const authMiddleware = require("../middleware/authMiddleware");
 
-const { createTask } = require("../controllers/taskControllers");
+const {
+  createTask,
+  getTasks,
+  getTask,
+  updateTask,
+  deleteTask,
+  updateTaskStatus,
+} = require("../controllers/taskControllers");
 
+// Create Task
 router.post("/", authMiddleware, createTask);
 
-module.exports = router;
+// Get All Tasks
+router.get("/", authMiddleware, getTasks);
 
+// Get Single Task
+router.get("/:id", authMiddleware, getTask);
+
+// Update Task
+router.put("/:id", authMiddleware, updateTask);
+
+// Delete Task
+router.delete("/:id", authMiddleware, deleteTask);
+
+// Update Status
+router.patch("/:id/status", authMiddleware, updateTaskStatus);
+
+module.exports = router;
