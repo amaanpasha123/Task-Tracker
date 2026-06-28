4f4f97f45e860fc31d1ab4e090c388ca96f1b0d9
Amaan Pasha <amaanpasha23@gmail.com>
2026-06-28 20:25:45 +0530

    made this finally

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://task-tracker-4og9.onrender.com",
+    baseURL: "https://task-tracker-4og9.onrender.com/api",
 });
 
 // Attach token to every request automatically
